eddb9330217de6cd0d669f79b570b5aa7303416c
Rod Gabrielle Cañete <98006488+RodCanete@users.noreply.github.com>
2023-04-06 05:54:03 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,118 @@
+'use strict';
+
+(function ($) {
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    $(document).ready(function() {
+        var phrases = ['Harmony', 'Serenity', 'Tranquility'];
+        var i = 0;
+        var intervalId = setInterval(function() {
+          $('.parallax-text span:last-child').removeClass('animate__fadeInDown').addClass('animate__fadeOutUp');
+          setTimeout(function() {
+            $('.parallax-text span:last-child').text(phrases[i]).removeClass('animate__fadeOutUp').addClass('animate__fadeInDown');
+            i++;
+            if (i >= phrases.length) {
+              i = 0;
+            }
+          }, 1000);
+        }, 3000);
+        setTimeout(function() {
+          $('.parallax-text').css('opacity', 1);
+        }, 500);
+      });
+    
+      
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Offcanvas Menu
+    $(".canvas-open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    // Search model
+    $('.search-switch').on('click', function () {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+        Hero Slider
+    --------------------*/
+   $(".hero-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        mouseDrag: false
+    });
+
+    /*------------------------
+		Testimonial Slider
+    ----------------------- */
+    $(".testimonial-slider").owlCarousel({
+        items: 1,
+        dots: false,
+        autoplay: true,
+        loop: true,
+        smartSpeed: 1200,
+        nav: true,
+        navText: ["<i class='arrow_left'></i>", "<i class='arrow_right'></i>"]
+    });
+
+    /*------------------
+        Magnific Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+    /*------------------
+		Date Picker
+	--------------------*/
+    $(".date-input").datepicker({
+        minDate: 0,
+        dateFormat: 'dd MM, yy'
+    });
+
+    /*------------------
+		Nice Select
+	--------------------*/
+    $("select").niceSelect();
+
+})(jQuery);
